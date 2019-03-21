8a435b2384301169d4312feb307c9bdc30e24f0e
cuipengyu <cuipy2004@163.com>
2019-03-21 13:03:35 +0800

    试用

--- WriterBot.py
+++ WriterBot.py
@@ -4,19 +4,22 @@
 from flask import Flask,jsonify,make_response,send_from_directory
 import synonyms
 import json
+import re
+import jieba
+import random
 
 app = Flask(__name__)
 
-@app.route('/test1/<word>',methods=['GET'])
-def test1(word):
+@app.route('/jinyi/<word>',methods=['GET','POST'])
+def jinyi(word):
     str = word
     lstseg = synonyms.seg(str)
-
     nword=''
     for i,str1 in enumerate(lstseg[0]):
-        print('%s = %s'%(str1,lstseg[1][i] ),synonyms.nearby(str1) )
+        # print('%s = %s'%(str1,lstseg[1][i] ),synonyms.nearby(str1) )
 
-        ignore_cixing = ['nz','nr']
+        # 忽略不处理的词性
+        ignore_cixing = ['x','nz','nr','eng','nrfg']
 
         if lstseg[1][i] in ignore_cixing:
             nstr = str1
@@ -24,10 +27,19 @@ def test1(word):
             nstr = _nearby_word(str1)
             if nstr == '':
                 nstr = str1
+            else:
+                irnd = random.randint(1,10)
+                if irnd<=1:
+                    nstr = str1
         nword+= nstr
 
-    print('原句：%s\n新句：%s'%(word,nword))
+    #print('原句：%s\n新句：%s'%(word,nword))
     return  make_response(nword)
+
+@app.route('/test2/<word>',methods=['GET'])
+def test2(word):
+    w = includeEngOrDigit(word)
+    return str(w)
 
 @app.route('/favicon.ico')
 def favicon():
@@ -42,7 +54,7 @@ def not_found(error):
 
 # 获得一个单词的最接近的同义词
 def _nearby_word(src_word):
-    if src_word is None or src_word=='':
+    if src_word is None or src_word=='' or includeEngOrDigit(src_word):
         return ''
     if len(src_word)<=1:
         return src_word
@@ -50,11 +62,18 @@ def _nearby_word(src_word):
     lstNearby = synonyms.nearby(src_word)
 
     if len(lstNearby)==2 and len(lstNearby[0])>2:
-        for nstr in lstNearby[0]:
-            if nstr!=src_word and len(nstr)==len(src_word):
-                return nstr
+        arr = lstNearby[0][1:]
+        res = random.choice( arr )
+        if res in ['</s>'] or includeEngOrDigit(res):
+            return ''
+        else:
+            return res
 
     return ''
+
+def includeEngOrDigit(word):
+    return not re.match('[a-zA-Z0-9_]',word) == None
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=5071)
